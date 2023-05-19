ca3da0555e83cade744d7813c9a0cb0a744f3652
0xPASTE <ycopyart.xyz@gmail.com>
2023-05-19 14:43:49 -0700

    ⛹️ made changes to the AU Player Controller

--- Assets/Scripts/AU_PlayerController.cs
+++ Assets/Scripts/AU_PlayerController.cs
@@ -3,16 +3,24 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.InputSystem;
 
+
 public class AU_PlayerController : MonoBehaviour
 {
+    [SerializeField] bool hasControl;
+    public static AU_PlayerController localPlayer;
+
     //Components
     Rigidbody myRB;
-    Transform myAvatar;
     Animator myAnim;
+    Transform myAvatar;
     //Player movement
     [SerializeField] InputAction WASD;
     Vector2 movementInput;
     [SerializeField] float movementSpeed;
+    //Player Color
+    static Color myColor;
+    SpriteRenderer myAvatarSprite;
+
 
 
     private void OnEnable()
@@ -31,29 +39,47 @@ public class AU_PlayerController : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
+        if (hasControl)
+        {
+            localPlayer = this;
+        }
+
         myRB = GetComponent<Rigidbody>();
-
-        myAvatar = transform.GetChild(0);
-
         myAnim = GetComponent<Animator>();
+        myAvatar = transform.GetChild(0);
+        myAvatarSprite = myAvatar.GetComponent<SpriteRenderer>();
+        if (myColor == Color.clear)
+            myColor = Color.white;
+        myAvatarSprite.color = myColor;
     }
 
     // Update is called once per frame
     void Update()
     {
         movementInput = WASD.ReadValue<Vector2>();
-
+        myAnim.SetFloat("Speed", movementInput.magnitude);
         if (movementInput.x != 0)
         {
             myAvatar.localScale = new Vector2(Mathf.Sign(movementInput.x), 1);
         }
 
-        myAnim.SetFloat("Speed", movementInput.magnitude);
     }
 
     private void FixedUpdate()
     {
         myRB.velocity = movementInput * movementSpeed;
     }
+
+    public void SetColor(Color newColor)
+    {
+        myColor = newColor;
+        if (myAvatarSprite != null)
+        {
+            myAvatarSprite.color = myColor;
+        }
+    }
+
+
+
 
 }
